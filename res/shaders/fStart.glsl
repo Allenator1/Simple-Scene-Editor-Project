302e9052a9fe1f11847a65f5f71d5193943c4be9
Allen Antony <22706998@student.uwa.edu.au>
2021-05-12 03:23:37 +0800

    Transferred calculation of lighting terms to fragment shader, to computer per fragment lighting

--- res/shaders/fStart.glsl
+++ res/shaders/fStart.glsl
@@ -1,9 +1,47 @@
-varying vec4 color;
 varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+varying vec3 Lvec;
+varying vec3 E;
+varying vec3 H;
+varying vec3 N;
+varying vec3 ambient;
 
 uniform sampler2D texture;
+uniform vec3 DiffuseProduct, SpecularProduct, AmbientProduct;
+uniform vec4 LightPosition;
+uniform float Shininess;
 
 void main()
-{
+{   
+    // Calculate distance attunement
+
+    float a = 1.0;              // constant term
+    float b = 0.7;              // linear term
+    float c = 1.8;              // quadratic term
+    float dist = length(Lvec);
+    float dist_attun = 1.0 / (a + b * dist + c * pow(dist, 2.0));
+
+    vec3 L = normalize(Lvec);     // Direction to the light source
+
+    // Compute terms in the illumination equation
+
+    vec3 ambient = AmbientProduct * dist_attun;
+
+    float Kd = max( dot(L, N), 0.0 ) * dist_attun;
+    vec3 diffuse = Kd * DiffuseProduct;
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess ) * dist_attun;
+    vec3 specular = Ks * SpecularProduct;
+    
+    if (dot(L, N) < 0.0 ) {
+	    specular = vec3(0.0, 0.0, 0.0);
+    } 
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+
+    vec4 color;
+    color.rgb = globalAmbient  + ambient + diffuse + specular;
+    color.a = 1.0; 
+
     gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
 }
--- res/shaders/vStart.glsl
+++ res/shaders/vStart.glsl
@@ -3,38 +3,41 @@ attribute vec3 vNormal;
 attribute vec2 vTexCoord;
 
 varying vec2 texCoord;
-varying vec4 color;
+varying vec3 Lvec;
+varying vec3 E;
+varying vec3 H;
+varying vec3 N;
 
-uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform mat4 ModelView;
 uniform mat4 Projection;
 uniform vec4 LightPosition;
-uniform float Shininess;
 
 void main()
 {
-    float a = 1.0;
-    float b = 0.7;
-    float c = 1.8;
-
     vec4 vpos = vec4(vPosition, 1.0);
 
     // Transform vertex position into eye coordinates
     vec3 pos = (ModelView * vpos).xyz;
 
     // The vector to the light from the vertex    
-    vec3 Lvec = LightPosition.xyz - pos;
-    float dist = length(Lvec);
-    float dist_attun = 1.0 / (a + b * dist + c * pow(dist, 2.0));
+    Lvec = LightPosition.xyz - pos;
+    vec3 L = normalize(Lvec);
 
     // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
-    vec3 E = normalize( -pos );   // Direction to the eye/camera
-    vec3 H = normalize( L + E );  // Halfway vector
 
-    // Transform vertex normal into eye coordinates (assumes scaling
-    // is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+    // Direction to the eye/camera 
+    E = normalize( -pos );     
+    // Halfway vector        
+    H = normalize( L + E );    
+    // Normal vector transformed to eye coordinates                             
+    N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+
+    gl_Position = Projection * ModelView * vpos;
+    texCoord = vTexCoord;
+
+    /* 
+    --- Compute the ambient, diffuse and specular terms in the vertex
+    shader ----- 
 
     // Compute terms in the illumination equation
     vec3 ambient = AmbientProduct * dist_attun;
@@ -52,8 +55,6 @@ void main()
     // globalAmbient is independent of distance from the light source
     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
     color.rgb = globalAmbient  + ambient + diffuse + specular;
-    color.a = 1.0;
-
-    gl_Position = Projection * ModelView * vpos;
-    texCoord = vTexCoord;
+    color.a = 1.0; 
+    */
 }
